4577a56987a2d0e805131fe7c5070834e27a1bba
Fernando Lopez <lopezfer@globalhitss.com>
2020-02-25 20:05:46 -0300

    se agrega dependencias

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule GraphqlElixir.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.4"},
+      {:absinthe_plug, "~> 1.4"},
+      {:dataloader, "~> 1.0"}
     ]
   end
 
